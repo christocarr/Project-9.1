47415c274ad9ef8e09ac1dc32619e538576b74d4
christocarr <christocarr@outlook.com>
2017-09-30 20:18:00 +0100

    charts added

--- main.js
+++ main.js
@@ -16,3 +16,122 @@ function alertCloseFunction () {
   let alertClose = closeButton.parentElement;
   alertClose.className += "close";
 }
+
+//Charts
+
+const trfChrt = document.getElementById('trafficChart');
+
+const dlyTrfChrt = document.getElementById('dailyTrafficChart');
+
+const mblUsrChrt = document.getElementById('mobileUserChart');
+
+//traffic chart
+
+let trafficChart = new Chart(trfChrt, {
+	type: 'line',
+	data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+						beginAtZero:true
+				}
+			}]
+		}
+	}
+});
+
+//daily traffic chart
+
+let dailyTrafficChart = new Chart(dlyTrfChrt, {
+	type: 'bar',
+	data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+						beginAtZero:true
+				}
+			}]
+		}
+	}
+});
+
+//mobile user chart
+
+let mobileUserChart = new Chart(mblUsrChrt, {
+    type: 'doughnut',
+    data: {
+			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+	},
+	options: {
+		
+	}
+});
