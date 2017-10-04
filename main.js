f45665ea3ed915086dc08e4a55350fdb58e383a2
christocarr <christocarr@outlook.com>
2017-10-04 20:15:07 +0100

    charts and css

--- main.js
+++ main.js
@@ -25,32 +25,25 @@ const dlyTrfChrt = document.getElementById('dailyTrafficChart');
 
 const mblUsrChrt = document.getElementById('mobileUserChart');
 
+Chart.defaults.global.legend.display = false;
+
 //traffic chart
 
 let trafficChart = new Chart(trfChrt, {
 	type: 'line',
 	data: {
-			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			labels: ["20-25", "26-30", "1-5", "6-10", "11-15", "16-20", "21-25", "26-31", "1-5"],
 			datasets: [{
-				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
+				label: '',
+				data: [],
 				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
 					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
 				],
 				borderColor: [
-					'rgba(255,99,132,1)',
 					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
 				],
-				borderWidth: 1
+				borderWidth: 1,
+				lineTension: 0
 			}]
 	},
 	options: {
@@ -63,6 +56,7 @@ let trafficChart = new Chart(trfChrt, {
 		}
 	}
 });
+
 
 //daily traffic chart
 
