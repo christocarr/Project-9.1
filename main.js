490e9a3a661f3264cafb289385c4248972bea758
Chris Carr <christocarr@outlook.com>
2017-10-05 21:28:25 +0100

    chart configuration

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ const dlyTrfChrt = document.getElementById('dailyTrafficChart');
 
 const mblUsrChrt = document.getElementById('mobileUserChart');
 
-Chart.defaults.global.legend.display = false;
+
+Chart.defaults.global.legend.display = false; //display charts with no legend
 
 //traffic chart
 
@@ -35,7 +36,7 @@ let trafficChart = new Chart(trfChrt, {
 			labels: ["20-25", "26-30", "1-5", "6-10", "11-15", "16-20", "21-25", "26-31", "1-5"],
 			datasets: [{
 				label: '',
-				data: [],
+				data: [0, 160, 200, 190, 240,150, 360, 280, 420],
 				backgroundColor: [
 					'rgba(54, 162, 235, 0.2)',
 				],
@@ -63,25 +64,27 @@ let trafficChart = new Chart(trfChrt, {
 let dailyTrafficChart = new Chart(dlyTrfChrt, {
 	type: 'bar',
 	data: {
-			labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+			labels: ["S", "M", "T", "W", "T", "F", "S"],
 			datasets: [{
 				label: '# of Votes',
-				data: [12, 19, 3, 5, 2, 3],
+				data: [50, 80, 150, 100, 200, 170, 90],
 				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
 					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(54, 162, 235, 0.2)'
 				],
 				borderColor: [
-					'rgba(255,99,132,1)',
 					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
+					'rgba(54, 162, 235, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(54, 162, 235, 1)'
 				],
 				borderWidth: 1
 			}]
@@ -89,8 +92,14 @@ let dailyTrafficChart = new Chart(dlyTrfChrt, {
 	options: {
 		scales: {
 			yAxes: [{
+				gridLines: {
+						drawTicks: false,
+						offsetGridLines: true
+				},
 				ticks: {
-						beginAtZero:true
+						beginAtZero:false,
+						stepSize: 50,
+						min: 0,
 				}
 			}]
 		}
